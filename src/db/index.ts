655ff6cdac37a50318ebbcf0a45000c580b86736
Drummonds <drummond05@proton.md>
2025-11-21 05:21:52 -0300

    feat: Implement Drizzle ORM with initial user schema, database migrations, and environment variable handling.

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,12 @@
-import { drizzle } from "drizzle-orm/better-sqlite3";
-import Database from "better-sqlite3";
+import { BetterSQLite3Database, drizzle } from "drizzle-orm/better-sqlite3";
+import DatabaseDriver from "better-sqlite3";
 import * as schema from "./schema";
+import { envCheck } from "../shared/helper/env-check";
 
-const sqlite = new Database("sqlite.db");
-export const db = drizzle(sqlite, { schema });
+export class DataBase {
+  public connection: BetterSQLite3Database<typeof schema>;
+  constructor() {
+    const sqlite = new DatabaseDriver(envCheck().DB_FILE_NAME);
+    this.connection = drizzle(sqlite, { schema });
+  }
+}
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,11 +1,18 @@
-import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
+import { sqliteTable, text, integer, check } from "drizzle-orm/sqlite-core";
+import { sql } from "drizzle-orm";
 import crypto from "crypto";
 
-export const users = sqliteTable("users", {
-  id: text("id").primaryKey().default(crypto.randomUUID()),
-  name: text("name").notNull(),
-  email: text("email").notNull().unique(),
-  createdAt: integer("created_at", { mode: "timestamp" }).$defaultFn(
-    () => new Date(),
-  ),
-});
+export const users = sqliteTable(
+  "users",
+  {
+    id: text("id")
+      .primaryKey()
+      .$defaultFn(() => crypto.randomUUID()),
+    name: text("name").notNull(),
+    email: text("email").notNull().unique(),
+    password_hash: text("password_hash").notNull(),
+    role: text("role", { enum: ["USER", "ADMIN"] }).default("USER"),
+    createdAt: integer("created_at", { mode: "timestamp" }).$defaultFn(() => new Date()),
+  },
+  (table) => [check("role_check", sql`${table.role} IN ('USER', 'ADMIN')`)]
+);
