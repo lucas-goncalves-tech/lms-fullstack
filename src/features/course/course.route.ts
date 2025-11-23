1cb1b8a21a07924d347ff0bb02e77e4413627ce8
Drummonds <drummond05@proton.md>
2025-11-23 17:08:19 -0300

    feat: add course and lesson routing with validation, session, and role-based authentication middleware

--- src/features/course/course.route.ts
+++ src/features/course/course.route.ts
@@ -5,26 +5,42 @@ import { CourseRepository } from "./course.repository";
 import { CourseService } from "./course.service";
 import { validateMiddleware } from "../../shared/middlewares/validate.middleware";
 import { createCourseSchema } from "./dto/create-course.dto";
+import { ValidateSessionMiddleware } from "../../shared/middlewares/validate-session.middleware";
+import { SessionsService } from "../sessions/sessions.service";
+import { SessionsRepository } from "../sessions/sessions.repository";
+import { CryptoService } from "../../shared/security/crypto-service.security";
+import { UserRepository } from "../user/user.repository";
+import { GuardRoleMiddleware } from "../../shared/middlewares/guard-role.middleware";
 
 export class CourseRoutes {
   private readonly controller: CourseController;
   private readonly router: Router;
+  private readonly validateSessionMiddleware: ValidateSessionMiddleware;
+  private readonly guardRoleMiddleware: GuardRoleMiddleware;
 
   constructor(private readonly db: DataBase) {
     const repository = new CourseRepository(this.db);
+    const sessionsRepository = new SessionsRepository(this.db);
+    const userRepository = new UserRepository(this.db);
+    const cryptoService = new CryptoService();
+    const sessionsService = new SessionsService(sessionsRepository, userRepository, cryptoService);
     const service = new CourseService(repository);
+    this.validateSessionMiddleware = new ValidateSessionMiddleware(sessionsService);
+    this.guardRoleMiddleware = new GuardRoleMiddleware();
     this.controller = new CourseController(service);
     this.router = Router();
     this.initRoutes();
   }
 
   private initRoutes() {
-    this.router.get("/", this.controller.findAll);
+    this.router.use(this.validateSessionMiddleware.validateSession);
     this.router.post(
       "/",
       validateMiddleware({ body: createCourseSchema }),
+      this.guardRoleMiddleware.guard(["ADMIN"]),
       this.controller.createCourse
     );
+    this.router.get("/", this.controller.findAll);
     this.router.get("/:courseSlug", this.controller.findBySlug);
   }
 
--- src/features/lessons/lesson.route.ts
+++ src/features/lessons/lesson.route.ts
@@ -7,21 +7,36 @@ import { Router } from "express";
 import { validateMiddleware } from "../../shared/middlewares/validate.middleware";
 import { createLessonSchema } from "./dto/create-lesson.dto";
 import { createLessonParamsSchema, findLessonParamsSchema } from "./dto/lesson-params.dto";
+import { SessionsRepository } from "../sessions/sessions.repository";
+import { UserRepository } from "../user/user.repository";
+import { CryptoService } from "../../shared/security/crypto-service.security";
+import { SessionsService } from "../sessions/sessions.service";
+import { ValidateSessionMiddleware } from "../../shared/middlewares/validate-session.middleware";
+import { GuardRoleMiddleware } from "../../shared/middlewares/guard-role.middleware";
 
 export class LessonRoutes {
   private readonly controller: LessonController;
   private readonly router: Router;
+  private readonly validateSessionMiddleware: ValidateSessionMiddleware;
+  private readonly guardRoleMiddleware: GuardRoleMiddleware;
 
   constructor(private readonly db: DataBase) {
     const repository = new LessonRepository(this.db);
     const courseRepository = new CourseRepository(this.db);
+    const sessionsRepository = new SessionsRepository(this.db);
+    const userRepository = new UserRepository(this.db);
+    const cryptoService = new CryptoService();
+    const sessionsService = new SessionsService(sessionsRepository, userRepository, cryptoService);
     const service = new LessonService(repository, courseRepository);
+    this.validateSessionMiddleware = new ValidateSessionMiddleware(sessionsService);
+    this.guardRoleMiddleware = new GuardRoleMiddleware();
     this.controller = new LessonController(service);
     this.router = Router({ mergeParams: true });
     this.initRoutes();
   }
 
   private initRoutes() {
+    this.router.use(this.validateSessionMiddleware.validateSession);
     this.router.get(
       "/",
       validateMiddleware({ params: createLessonParamsSchema }),
@@ -38,6 +53,7 @@ export class LessonRoutes {
     this.router.post(
       "/",
       validateMiddleware({ params: createLessonParamsSchema, body: createLessonSchema }),
+      this.guardRoleMiddleware.guard(["ADMIN"]),
       this.controller.createLesson
     );
   }
