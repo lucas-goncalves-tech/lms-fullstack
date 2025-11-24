dc06f75852925d5165be6923ff6bc8a60ed91511
Drummonds <drummond05@proton.md>
2025-11-24 09:47:40 -0300

    feat: drizzle kit with dot env

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "drizzle-kit";
+import { config } from "dotenv";
+config({ path: ".env.local" });
 
 export default defineConfig({
   schema: "./src/db/schema/index.ts",
   out: "./drizzle",
   dialect: "sqlite",
   dbCredentials: {
-    url: "file:./" + process.env.DB_FILE_NAME,
+    url: process.env.DB_FILE_NAME!,
   },
 });
