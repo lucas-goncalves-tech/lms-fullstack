03fb56cf8bece67d1eaea908ba3b4cada0cab507
Drummonds <drummond05@proton.md>
2025-11-24 06:04:04 -0300

    remove: static html endpoint

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,6 @@ import { errorHandler } from "./shared/middlewares/error.middleware";
 import { DataBase } from "./db";
 import { MainRoutes } from "./route";
 import { NotfoundError } from "./shared/errors/not-found.error";
-import path from "node:path";
 import cookieParser from "cookie-parser";
 class App {
   public app: express.Express;
@@ -17,18 +16,13 @@ class App {
   }
 
   private middlewares() {
-    this.app.use(
-      helmet()
-    );
+    this.app.use(helmet());
     this.app.use(cors());
     this.app.use(express.json());
     this.app.use(cookieParser());
   }
 
   private routes() {
-    // Serve arquivos estáticos da pasta web
-    this.app.use(express.static(path.join(__dirname, "../web")));
-    
     this.app.use("/api/v1", new MainRoutes(this.db).getRouter);
     this.app.use((_req, _res, next) => {
       next(new NotfoundError("Endpoint não encontrado"));
